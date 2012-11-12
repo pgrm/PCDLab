d4700bc5ae914806336bda172f6f2e759921878a
Peter Grman <peter.grman@gmail.com>
2012-11-12 03:31:06 +0100

    removed NetBeans projects

---
+++ Session2/src/session2/after/two_request_types/Main.java
@@ -0,0 +1,16 @@
+package session2.during.toun_council_office;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Main().run();
+	}
+
+	public void run() {
+		
+	}
+
+}
